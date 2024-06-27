f9506d70eebada3fe1b13f468e5e2970a1d886e8
Daniel <danielbrodowski97@gmail.com>
2024-06-27 15:04:39 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = new express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log('Server running on port 3000.');
+});
